d6f1b7771dc0cebc21194fde8770845aeca9b19d
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 19:06:15 +0600

    os fs module

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 // const _ = require("lodash");
+// path module
 const path = require("path");
+// os module
 const os = require("os");
+// fs module
+const fs = require("fs");
 
 const myPath = "D:/LWS_NODE_JS_EXPRESS_JS/index.js";
-console.log(path.basename(myPath));
-console.log(path.dirname(myPath));
-console.log(path.extname(myPath));
+// console.log(path.basename(myPath));
+// console.log(path.dirname(myPath));
+// console.log(path.extname(myPath));
 
-console.log(os.platform());
-console.log(os.homedir());
-console.log(os.freemem());
-console.log(os.totalmem());
-console.log(os.cpus());
+// console.log(os.platform());
+// console.log(os.homedir());
+// console.log(os.freemem());
+// console.log(os.totalmem());
+// console.log(os.cpus());
+
+// write file
+fs.writeFileSync("myFile.txt", "hello world \n");
+// append file
+fs.appendFileSync("myFile.txt", "hello programmers");
+
+// read file // buffer
+const data = fs.readFileSync("myFile.txt", "utf-8");
+console.log(data);
