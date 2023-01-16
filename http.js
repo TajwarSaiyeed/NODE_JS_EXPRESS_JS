fb3d808007822b141e67de75b0ae0712e23ebefa
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 20:24:53 +0600

    http module

--- http.js
+++ http.js
@@ -1,1 +1,24 @@
 const http = require("http");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("hello programmner\n");
+    res.write("Nice program");
+    res.end();
+  } else if (req.url === "/about") {
+    res.write("about page");
+    res.end();
+  } else {
+    res.write("404 page not found");
+    res.end();
+  }
+});
+
+// optional
+// server.on("connection", (socket) => {
+//   console.log("new connection...");
+// });
+
+server.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
