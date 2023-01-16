306c48368bab10ecf8efd836554e9997a306d62c
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 19:20:09 +0600

    event module

--- event.js
+++ event.js
@@ -4,13 +4,16 @@ const emitter = new EventEmitter();
 
 // register a listener for bellRing event
 
-emitter.on("bellRing", () => {
-  console.log("a student entered the class");
+emitter.on("bellRing", ({ period, text }) => {
+  console.log(`we need to run because ${period} ${text}`);
 });
 
 // raise an event
 // emitter.emit("bellRing");
 
 setTimeout(() => {
-  emitter.emit("bellRing");
+  emitter.emit("bellRing", {
+    period: "First",
+    text: "period ended",
+  });
 }, 2000);
