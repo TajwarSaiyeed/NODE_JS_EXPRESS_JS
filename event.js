75856aa00040362889782ea7cafb98c323e33f81
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 19:16:43 +0600

    event module

--- event.js
+++ event.js
@@ -1,1 +1,12 @@
 const EventEmitter = require("events");
+
+const emitter = new EventEmitter();
+
+// register a listener for bellRing event
+
+emitter.on("bellRing", () => {
+  console.log("a student entered the class");
+});
+
+// raise an event
+emitter.emit("bellRing");
