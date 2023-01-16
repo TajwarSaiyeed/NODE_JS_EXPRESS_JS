8b8efd0e69823dce48321a3ce3347df85accc8b2
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 18:59:04 +0600

    path module

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const _ = require("lodash");
+// const _ = require("lodash");
+const path = require("path");
+
+const myPath = "D:/LWS_NODE_JS_EXPRESS_JS/index.js";
+console.log(path.basename(myPath));
+console.log(path.dirname(myPath));
+console.log(path.extname(myPath));
