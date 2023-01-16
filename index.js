2d17e0b0e819ce0b44474e856b703762c5d4e34e
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 19:01:42 +0600

    os module

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 // const _ = require("lodash");
 const path = require("path");
+const os = require("os");
 
 const myPath = "D:/LWS_NODE_JS_EXPRESS_JS/index.js";
 console.log(path.basename(myPath));
 console.log(path.dirname(myPath));
 console.log(path.extname(myPath));
+
+console.log(os.platform());
+console.log(os.homedir());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.cpus());
