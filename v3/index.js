90d4f25f6d99302617528b134dfc381463362ed0
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 20:43:09 +0600

    stream buffer module

--- v3/index.js
+++ v3/index.js
@@ -3,5 +3,7 @@ const fs = require("fs");
 const ourReadSteam = fs.createReadStream(`${__dirname}/bigdata.txt`, "utf-8");
 
 ourReadSteam.on("data", (data) => {
-  console.log(data);
+  console.log("data");
 });
+
+console.log("hello");
