6b96d76ddb61184509171fee5d7266488f267798
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 21:07:03 +0600

    stream buffer read write pipe stream  module

--- v3/index.js
+++ v3/index.js
@@ -3,8 +3,10 @@ const fs = require("fs");
 const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
 const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on("data", (data) => {
-  ourWriteStream.write(data);
-});
+// ourReadStream.on("data", (data) => {
+//   ourWriteStream.write(data);
+// });
+
+ourReadStream.pipe(ourWriteStream);
 
 // console.log("hello");
