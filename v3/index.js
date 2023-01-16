0cfd5986c2defdb1195d29f9596cc2391af725c2
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-16 21:00:07 +0600

    stream buffer read stream  module

--- v3/index.js
+++ v3/index.js
@@ -7,35 +7,3 @@
 // });
 
 // console.log("hello");
-
-const http = require("http");
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write(`<html><head></head></head>`);
-    res.write(
-      `<body><form action="/process" method="POST"><input name="message" /></form></body>`
-    );
-    res.end();
-  } else if (req.url === "/process" && req.method === "POST") {
-    const body = [];
-    req.on("data", (chank) => {
-      body.push(chank);
-    });
-
-    req.on("end", () => {
-      console.log("Stream Finished");
-      const parseBody = Buffer.concat(body).toString();
-      console.log(parseBody.split("+").join(" "));
-    });
-    res.write("Thank you for submitting");
-    res.end();
-  } else {
-    res.write("404 page not found");
-    res.end();
-  }
-});
-
-server.listen(5000, () => {
-  console.log("server is listening on port 5000...");
-});
